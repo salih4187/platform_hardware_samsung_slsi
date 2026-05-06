f4a32209ec82c85166debb3af89d0fe5287ddc6a
Salih Çağlar <salihcaglar4187@gmail.com>
2026-05-06 22:58:59 +0300

    Fix CFLAGS makefile paths for gralloc0

--- exynos/gralloc/Android.mk
+++ exynos/gralloc/Android.mk
@@ -69,7 +69,7 @@ LOCAL_CFLAGS += -DGRALLOC_MSCL_ALIGN_RESTRICTION
 LOCAL_CFLAGS += -DGRALLOC_10B_ALIGN_RESTRICTION
 endif
 
-include $(TOP)/hardware/samsung_slsi-linaro/exynos/BoardConfigCFlags.mk
+include $(TOP)/hardware/samsung_slsi/exynos/BoardConfigCFlags.mk
 include $(BUILD_SHARED_LIBRARY)
 
 #######################################################################################################
